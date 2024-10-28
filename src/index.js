e45e0c1b94bd66dea1ee1aeaab490f862898deac
ayomideRussianCode <ayindeayomide7@gmail.com>
2024-10-28 10:43:00 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'normalize.css';
+import "normalize.css";
 import App from "./App";
-import {GlobalStyles} from "./global-styles";
+import { GlobalStyles } from "./global-styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
